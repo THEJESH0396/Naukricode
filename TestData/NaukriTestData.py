fe3964d0da5cf5ea969a6591ba3873a7c0d31305
THEJESH0396 <tej.tlp@gmail.com>
2024-06-22 00:06:14 +0530

    code changes

--- TestData/NaukriTestData.py
+++ TestData/NaukriTestData.py
@@ -2,4 +2,4 @@ class HomePageData:
 
     test_HomePage_data = [{"username": "thej.tlp@gmail.com", "password": "Thej1234@@"}]
 
-    test_HomePage_data1 = "To develop my career as a Senior Quality Analyst/ Test Lead in a growth oriented organization which provides a professional environment to apply my knowledge of testing skills for the growth of the organization and acquire knowledge and new skills."
+    test_HomePage_data1 = "To develop my career as a Senior Quality Analyst/Test Lead in a growth oriented organization which provides a professional environment to apply my knowledge of testing skills for the growth of the organization and acquire knowledge and new skills."
